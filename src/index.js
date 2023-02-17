6cbbe74249d20dff51d37dce0dd908a5dc7c7fd1
YanaB-b <j.biliuk96@gmail.com>
2023-02-17 11:24:02 +0100

    1

--- src/index.js
+++ src/index.js
@@ -34,11 +34,12 @@ function createMarkup(article) {
     } else if (article.length === 1) {
         creare = article.map(({name:{official},flags:{svg},capital, population, languages}) =>{
             const value = Object.values(languages);
-            return 
-            `<p class="svg"><img width="40px" height="20px" src=${svg}>${official}</p>
+            return `
+            <p class="svg"><img width="40px" height="20px" src=${svg}>${official}</p>
 <p class = "info">capital: <span class ="text">${capital}</span></p>
 <p class = "info">population: <span class ="text">${population}</span></p>
-<p class = "info">languages: <span class ="text">${value}</span></p>`
+<p class = "info">languages: <span class ="text">${value}</span></p>
+`;
 
         }
         )
@@ -48,8 +49,9 @@ function createMarkup(article) {
 
     }else {
         creare = article.map(({name: {official}, flags: { svg }})=> 
-       { return 
-        `<li><img width="40px" height="20px" src=${svg}>${official}</li>`;
+       { return `
+       <li><img width="40px" height="20px" src=${svg}>${official}</li>
+       `;
      } ).join('');
      toClearPage();
      list.insertAdjacentHTML('beforeend', creare);
