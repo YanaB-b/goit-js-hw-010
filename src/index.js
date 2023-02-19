82ac96d31bc828cb9aafb2981c6c5de5ba493f57
YanaB-b <j.biliuk96@gmail.com>
2023-02-19 18:09:39 +0100

    1

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function onSubmit(e) {
         toClearPage();
         return;
     }
-fetchCountries(input).then(createMarkup).catch(onError);
+fetchCountries(value).then(createMarkup).catch(onError);
 
 }
 function createMarkup(article) {
