bb2f84764b59c1d756121c6b2e934ed93bfd5521
YanaB-b <j.biliuk96@gmail.com>
2023-02-19 18:27:59 +0100

    +

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const URL = 'https://restcountries.com/v3.1/name';
 
 
 function fetchCountries(name) {
-    return fetch (`${URL}/${name}?frields=name,capital,population,flags,languages`).then(response => {
+    return fetch (`${URL}/${name}?fields=name,capital,population,flags,languages`).then(response => {
         if (!response.ok){
             throw new Error(response.status);
         }
